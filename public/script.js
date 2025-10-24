6d4423713f1f2a9947f43f8c6b85c3b683f55d67
Jorge Carlos Quevedo <jorgecarlosquevedo@gmail.com>
2025-10-24 12:55:35 -0700

    added parallax effect

--- public/script.js
+++ public/script.js
@@ -202,7 +202,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // Initialize carousel
     showSlide(0);
-    startCarousel();
+    // startCarousel(); // DISABLED for responsive testing
 
     // Thumbnail click handlers
     thumbnails.forEach((thumbnail, index) => {
